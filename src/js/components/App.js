41f2a96826807b0778d7bad7ac0b968b23736f80
Chip Irek <chip.irek@gmail.com>
2018-02-17 09:07:50 -0500

    Starting to add material-ui components

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,18 +1,25 @@
 import React from "react";
 import List from "./List";
 import Form from "./Form";
+import MDemo from './MDemo';
+import Header from './Header';
+
+const data = {
+  menus: [
+    { text: 'Today', link: '/ListPage?dt=2018-02-01' },
+    { text: 'Tomorrow', link: '/ListPage?dt=2018-02-03' },
+  ],
+}
 
 const App = () => (
-  <div className="row mt-5">
-    <div className="col-md-4 offset-md-1">
-      <h2>Tasks</h2>
-      <List />
-    </div>
-    <div className="col-md-4 offset-md-1">
-      <h2>Add a new task</h2>
-      <Form />
-    </div>
+
+  <div>
+    <Header />
+    <MDemo />
   </div>
+
+
+  
 );
 
 export default App;
--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,19 @@ import { render } from "react-dom";
 import { Provider } from "react-redux";
 import configureStore from "../js/store";
 import App from "../js/components/App";
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import ThemeDefault from './theme-default';
 
 const store = configureStore();
 
+injectTapEventPlugin();
+
 render(
   <Provider store={store}>
-    <App />
+        <MuiThemeProvider muiTheme={ThemeDefault} >
+              <App />
+        </MuiThemeProvider>
   </Provider>,
   document.getElementById("app")
 );
