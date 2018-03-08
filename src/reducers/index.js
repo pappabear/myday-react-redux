7eb840f3f0c757c17f2d33b478ef6fb5576ec981
Chip Irek <chip.irek@gmail.com>
2018-03-08 05:11:41 -0600

    Removed redux-form from packages and reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import { tasks, tasksHasErrored, tasksIsLoading } from './tasks';
-import { reducer as reduxFormReducer } from 'redux-form';
 
 export default combineReducers({
     tasks,
     tasksHasErrored,
-    tasksIsLoading,
-    form: reduxFormReducer
+    tasksIsLoading
 });
